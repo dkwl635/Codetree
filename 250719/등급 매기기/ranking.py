d9575b5870cebe5cb0474cbadf5af2f4cad51df3
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-07-19 20:11:51 +0900

    Title: 등급 매기기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250719/등급 매기기/ranking.py
@@ -0,0 +1,18 @@
+#input value
+N = int(input())
+
+#N >= 90 A
+if N >= 90: 
+    print('A')
+#N >= 80 B
+elif N >= 80:
+    print('B')
+#N >= 70 C
+elif N >= 70:
+    print('C')
+#N >= 60 D
+elif N >= 60:
+    print('D')
+#else F
+else:
+    print('F')
