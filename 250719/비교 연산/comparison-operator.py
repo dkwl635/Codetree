6d03055317ef08c115007bb849b53e8a54556c25
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-07-19 19:33:14 +0900

    Title: 비교 연산, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250719/비교 연산/comparison-operator.py
@@ -0,0 +1,13 @@
+A,B = map(int, input().split())
+#A >= B
+print(int(A>=B))
+#A > B
+print(int(A>B))
+#B >= A
+print(int(B>=A))
+#B > A
+print(int(B>A))
+#A == B
+print(int(A==B))
+#A != B
+print(int(A!=B))
