24bc333990bc49480bdafb21c5396e66b0f676a2
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-07-21 19:38:40 +0900

    Title: 간단한 사칙연산, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250721/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A , B;
+    cin >> A >> B;
+    cout << A +B << "\n" << A - B << "\n" <<
+    A / B << "\n" << A % B;
+
+    return 0;
+}
