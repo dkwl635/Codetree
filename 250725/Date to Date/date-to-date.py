5d1812efcdea46372981a66844182036e9a90bae
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-07-25 19:32:06 +0900

    Title: Date to Date, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250725/Date to Date/date-to-date.py
+++ 250725/Date to Date/date-to-date.py
@@ -5,16 +5,14 @@ m1, d1, m2, d2 = map(int, input().split())
 #                 1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-total_A = 0;
-for i in range(1, m1) :
-    total_A += num_of_days[i]
 
-total_A += d1
+cnt = 1
+while not(m1 == m2 and d1 == d2) :
+    d1 +=1
+    cnt +=1
+    if d1 > num_of_days[m1] :
+        m1 +=1
+        d1 = 1
 
-total_B = 0;
-for i in range(1, m2) :
-    total_B += num_of_days[i]
 
-total_B += d2
-
-print(total_B - total_A + 1)
+print(cnt)
