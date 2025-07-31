cfae27d58b2cacdf2a7a75c4e663000189e5c8cd
김재완 <82872150+dkwl635@users.noreply.github.com>
2025-07-31 20:23:55 +0900

    Title: 야바위, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250731/야바위/ya-rock.py
@@ -0,0 +1,24 @@
+n = int(input())
+moves = [tuple(map(int, input().split())) for _ in range(n)]
+a, b, c = zip(*moves)
+a, b, c = list(a), list(b), list(c)
+
+# Please write your code here.
+MAX = 0
+for i in range(1,3) :
+
+    answer = i
+    cnt = 0
+    for j in range (n) :
+        if a[j] == answer :
+            answer = b[j] 
+        elif b[j] == answer :
+            answer = a[j]
+
+        if answer == c[j] :
+            cnt += 1
+
+    if MAX < cnt :
+        MAX = cnt
+
+print(MAX)
